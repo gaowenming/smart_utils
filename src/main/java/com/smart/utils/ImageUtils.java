b960ea61adb925dbef751b17beb21c419b8a3fca
gaowenming <gaowm0207@163.com>
2015-08-22 20:40:13 +0800

    remove logback

--- src/main/java/com/smart/utils/ImageUtils.java
+++ src/main/java/com/smart/utils/ImageUtils.java
@@ -27,8 +27,6 @@ import java.io.OutputStream;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
-import org.apache.log4j.Logger;
-
 /**
  * Description: <图片加水印工具类>. <br>
  * <p>
@@ -44,10 +42,7 @@ public class ImageUtils {
 	public static Integer TYPE_TEXT = 0;
 	public static Integer TYPE_IMAGE = 1;
 
-	static Logger logger = Logger.getLogger(ImageUtils.class);
-
-	private static void drawImage(Graphics2D g, String iconPath,
-			Integer buffImgWidth, Integer buffImgHeight, Float clarity) {
+	private static void drawImage(Graphics2D g, String iconPath, Integer buffImgWidth, Integer buffImgHeight, Float clarity) {
 		// 水印图象的路径 水印一般为gif或者png的，这样可设置透明度
 		ImageIcon imgIcon = new ImageIcon(iconPath);
 		int width = imgIcon.getIconWidth();
@@ -70,8 +65,7 @@ public class ImageUtils {
 		BufferedImage buffImgIcon = zoomImage(iconPath, scale);
 
 		float alpha = clarity; // 透明度
-		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,
-				alpha));
+		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, alpha));
 
 		int logoTextx, logoTexty;
 		// logotext默认上下左右居中显示
@@ -84,8 +78,7 @@ public class ImageUtils {
 
 	}
 
-	private static void drawString(Graphics2D g, String logoText,
-			Integer buffImgWidth, Integer buffImgHeight, Float clarity) {
+	private static void drawString(Graphics2D g, String logoText, Integer buffImgWidth, Integer buffImgHeight, Float clarity) {
 
 		// 创建字体
 		Font font = null;
@@ -98,12 +91,9 @@ public class ImageUtils {
 			FontRenderContext context = g.getFontRenderContext();
 			// 获取字体的像素范围对象
 			Rectangle2D stringBounds = font.getStringBounds(logoText, context);
-			int width = Integer.parseInt(new java.text.DecimalFormat("0")
-					.format(stringBounds.getWidth()));
+			int width = Integer.parseInt(new java.text.DecimalFormat("0").format(stringBounds.getWidth()));
 			if (buffImgWidth < width) {
 				font = new Font("宋体", Font.BOLD, i - 1);
-				logger.debug("i:" + i + " -->buffImgWidth:" + buffImgWidth
-						+ " : " + width);
 				break;
 			}
 		}
@@ -114,16 +104,11 @@ public class ImageUtils {
 		Rectangle2D stringBounds = font.getStringBounds(logoText, context);
 		// 设置文字透明度变量
 		float alpha = clarity;
-		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,
-				alpha));
+		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, alpha));
 		int logoTextx, logoTexty;
 		// logotext默认上下左右居中显示
-		logoTextx = (buffImgWidth - Integer
-				.parseInt(new java.text.DecimalFormat("0").format(stringBounds
-						.getWidth()))) / 2;
-		logoTexty = (buffImgHeight - Integer
-				.parseInt(new java.text.DecimalFormat("0").format(stringBounds
-						.getHeight()))) / 2;
+		logoTextx = (buffImgWidth - Integer.parseInt(new java.text.DecimalFormat("0").format(stringBounds.getWidth()))) / 2;
+		logoTexty = (buffImgHeight - Integer.parseInt(new java.text.DecimalFormat("0").format(stringBounds.getHeight()))) / 2;
 		// 第一参数->设置的内容，后面两个参数->文字在图片上的坐标位置(x,y) .
 		g.drawString(logoText, logoTextx, logoTexty);
 		g.dispose();
@@ -134,8 +119,7 @@ public class ImageUtils {
 	 * 描述 : <图片添加水印功能,水印可以是文字或者图片>. <br>
 	 * <p>
 	 * 
-	 * 图片都会按照png格式输出 请注意
-	 * <水印居中显示 中心对称旋转, 水印文字或者水印图片 会被按比例缩放,
+	 * 图片都会按照png格式输出 请注意 <水印居中显示 中心对称旋转, 水印文字或者水印图片 会被按比例缩放,
 	 * 水印的最长边所放到原始图片的最短边小一点(因为图片水印和文字水印缩放的修正值可能有偏差)>
 	 * </p>
 	 * 
@@ -153,10 +137,8 @@ public class ImageUtils {
 	 *            CP_WatermarkUtils.TYPE_IMAGE:图片水印
 	 *            CP_WatermarkUtils.TYPE_TEXT:文字水印
 	 */
-	public static void waterMark(String waterMarkContent, String srcImgPath,
-			String targerPath, Integer degree, Float clarity, Integer type) {
+	public static void waterMark(String waterMarkContent, String srcImgPath, String targerPath, Integer degree, Float clarity, Integer type) {
 		if (!new File(srcImgPath).exists()) {
-			logger.error("CP_WatermarkUtils -> waterMarImagekByText异常:图片源文件不存在!");
 			return;
 		}
 		if (clarity == null) {
@@ -170,8 +152,7 @@ public class ImageUtils {
 			Image srcImg = ImageIO.read(new File(srcImgPath));
 
 			// 根据image创建bufferedimage对象
-			BufferedImage buffImg = new BufferedImage(srcImg.getWidth(null),
-					srcImg.getHeight(null), BufferedImage.TYPE_INT_RGB);
+			BufferedImage buffImg = new BufferedImage(srcImg.getWidth(null), srcImg.getHeight(null), BufferedImage.TYPE_INT_RGB);
 
 			int buffImgWidth = buffImg.getWidth();
 			int buffImgHeight = buffImg.getHeight();
@@ -179,53 +160,43 @@ public class ImageUtils {
 			Graphics2D g = buffImg.createGraphics();
 
 			// 设置对线段的锯齿状边缘处理
-			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-					RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 			// 将srcImage原图像 画到buffimg中
-			g.drawImage(
-					srcImg.getScaledInstance(srcImg.getWidth(null),
-							srcImg.getHeight(null), Image.SCALE_SMOOTH), 0, 0,
-					null);
+			g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0, 0, null);
 
 			if (null != degree) {
 				// 设置水印旋转
-				g.rotate(Math.toRadians(degree), (double) buffImgWidth / 2,
-						(double) buffImgHeight / 2);
+				g.rotate(Math.toRadians(degree), (double) buffImgWidth / 2, (double) buffImgHeight / 2);
 			}
 
 			// 设置颜色
 			g.setColor(Color.red);
 
 			if (type == TYPE_TEXT) {
-				drawString(g, waterMarkContent, buffImgWidth, buffImgHeight,
-						clarity);
+				drawString(g, waterMarkContent, buffImgWidth, buffImgHeight, clarity);
 			} else if (type == TYPE_IMAGE) {
-				drawImage(g, waterMarkContent, buffImgWidth, buffImgHeight,
-						clarity);
+				drawImage(g, waterMarkContent, buffImgWidth, buffImgHeight, clarity);
 			}
 			os = new FileOutputStream(targerPath);
 
 			// 生成图片
 			ImageIO.write(buffImg, "PNG", os);
 
-			logger.info("图片添加文字水印完成!");
+			System.out.println("图片添加文字水印完成!");
 		} catch (Exception e) {
-			logger.error("CP_WatermarkUtils --> waterMarImagekByText图片添加水印异常:",
-					e);
+			System.out.println("CP_WatermarkUtils --> waterMarImagekByText图片添加水印异常:" + e);
 		} finally {
 			try {
 				if (null != is)
 					is.close();
 			} catch (Exception e) {
-				logger.error(
-						"CP_WatermarkUtils --> waterMarImagekByText关闭流异常:", e);
+				System.out.println("CP_WatermarkUtils --> waterMarImagekByText关闭流异常:" + e);
 			}
 			try {
 				if (null != os)
 					os.close();
 			} catch (Exception e) {
-				logger.error(
-						"CP_WatermarkUtils --> waterMarImagekByText关闭流异常:", e);
+				System.out.println("CP_WatermarkUtils --> waterMarImagekByText关闭流异常:" + e);
 			}
 		}
 	}
@@ -234,8 +205,7 @@ public class ImageUtils {
 	 * 
 	 * 描述 : <图片添加图片水印功能>. <br>
 	 * <p>
-	 * 图片都会按照png格式输出 请注意
-	 * <水印居中显示 中心对称旋转, 水印图片 会被按比例缩放,
+	 * 图片都会按照png格式输出 请注意 <水印居中显示 中心对称旋转, 水印图片 会被按比例缩放,
 	 * 水印的最长边所放到原始图片的最短边小一点(因为图片水印和文字水印缩放的修正值可能有偏差)>
 	 * </p>
 	 * 
@@ -249,22 +219,18 @@ public class ImageUtils {
 	 *            水印图片旋转角度
 	 * @param clarity
 	 *            透明度(小于1的数)越接近0越透明 默认为0.2
-	 *            
+	 * 
 	 */
-	public static void waterMarkByImage(String waterMarkContent, String srcImgPath,
-			String targerPath, Integer degree, Float clarity) {
-		waterMark(waterMarkContent, srcImgPath, targerPath, degree, clarity,ImageUtils.TYPE_IMAGE);
+	public static void waterMarkByImage(String waterMarkContent, String srcImgPath, String targerPath, Integer degree, Float clarity) {
+		waterMark(waterMarkContent, srcImgPath, targerPath, degree, clarity, ImageUtils.TYPE_IMAGE);
 	}
+
 	/**
 	 * 
 	 * 描述 : <图片添加图片水印功能>. <br>
 	 * <p>
-	 * 图片都会按照png格式输出 请注意
-	 * <水印居中显示 中心对称旋转, 水印图片 会被按比例缩放,
-	 * 水印的最长边所放到原始图片的最短边小一点(因为图片水印和文字水印缩放的修正值可能有偏差)
-	 * 默认旋转角度为水平
-	 * 默认透明度为0.2f
-	 * >
+	 * 图片都会按照png格式输出 请注意 <水印居中显示 中心对称旋转, 水印图片 会被按比例缩放,
+	 * 水印的最长边所放到原始图片的最短边小一点(因为图片水印和文字水印缩放的修正值可能有偏差) 默认旋转角度为水平 默认透明度为0.2f >
 	 * </p>
 	 * 
 	 * @param waterMarkContent
@@ -274,16 +240,15 @@ public class ImageUtils {
 	 * @param targerPath
 	 *            目标图片路径
 	 */
-	public static void waterMarkByImage(String waterMarkContent, String srcImgPath,
-			String targerPath) {
-		waterMark(waterMarkContent, srcImgPath, targerPath, null, null,ImageUtils.TYPE_IMAGE);
+	public static void waterMarkByImage(String waterMarkContent, String srcImgPath, String targerPath) {
+		waterMark(waterMarkContent, srcImgPath, targerPath, null, null, ImageUtils.TYPE_IMAGE);
 	}
+
 	/**
 	 * 
 	 * 描述 : <图片添加文字水印功能>. <br>
 	 * <p>
-	 * 图片都会按照png格式输出 请注意
-	 * <水印居中显示 中心对称旋转, 水印文字 会被按比例缩放,
+	 * 图片都会按照png格式输出 请注意 <水印居中显示 中心对称旋转, 水印文字 会被按比例缩放,
 	 * 水印的最长边所放到原始图片的最短边小一点(因为图片水印和文字水印缩放的修正值可能有偏差)>
 	 * </p>
 	 * 
@@ -297,21 +262,18 @@ public class ImageUtils {
 	 *            水印文字旋转角度
 	 * @param clarity
 	 *            透明度(小于1的数)越接近0越透明 默认为0.2
-	 *            
+	 * 
 	 */
-	public static void waterMarkByText(String waterMarkContent, String srcImgPath,
-			String targerPath, Integer degree, Float clarity) {
-		waterMark(waterMarkContent, srcImgPath, targerPath, degree, clarity,ImageUtils.TYPE_TEXT);
+	public static void waterMarkByText(String waterMarkContent, String srcImgPath, String targerPath, Integer degree, Float clarity) {
+		waterMark(waterMarkContent, srcImgPath, targerPath, degree, clarity, ImageUtils.TYPE_TEXT);
 	}
+
 	/**
 	 * 
 	 * 描述 : <图片添加文字水印功能>. <br>
 	 * <p>
-	 * 图片都会按照png格式输出 请注意
-	 * <水印居中显示 中心对称旋转, 水印文字 会被按比例缩放,
-	 * 水印的最长边所放到原始图片的最短边小一点(因为图片水印和文字水印缩放的修正值可能有偏差)>
-	  * 默认旋转角度为水平
-	 * 默认透明度为0.2f
+	 * 图片都会按照png格式输出 请注意 <水印居中显示 中心对称旋转, 水印文字 会被按比例缩放,
+	 * 水印的最长边所放到原始图片的最短边小一点(因为图片水印和文字水印缩放的修正值可能有偏差)> 默认旋转角度为水平 默认透明度为0.2f
 	 * </p>
 	 * 
 	 * @param waterMarkContent
@@ -320,18 +282,17 @@ public class ImageUtils {
 	 *            源图片路径
 	 * @param targerPath
 	 *            目标图片路径
-	 *            
+	 * 
 	 */
-	public static void waterMarkByText(String waterMarkContent, String srcImgPath,
-			String targerPath) {
-		waterMark(waterMarkContent, srcImgPath, targerPath, null, null,ImageUtils.TYPE_TEXT);
+	public static void waterMarkByText(String waterMarkContent, String srcImgPath, String targerPath) {
+		waterMark(waterMarkContent, srcImgPath, targerPath, null, null, ImageUtils.TYPE_TEXT);
 	}
+
 	/**
 	 * 
 	 * 描述 : <按比例缩放图像>. <br>
 	 * <p>
-	 * 图片都会按照png格式输出 请注意
-	 * <使用方法说明>
+	 * 图片都会按照png格式输出 请注意 <使用方法说明>
 	 * </p>
 	 * 
 	 * @param srcPath
@@ -342,8 +303,7 @@ public class ImageUtils {
 	 *            缩放精度 底数为100, 例如:100为原始大小 200为放大两倍 50为缩小一倍
 	 * @throws IOException
 	 */
-	public static void zoomImage(String srcPath, String destPath, int scale)
-			throws IOException {
+	public static void zoomImage(String srcPath, String destPath, int scale) throws IOException {
 		ImageIO.write(zoomImage(srcPath, scale), "PNG", new File(destPath));
 	}
 
@@ -371,17 +331,15 @@ public class ImageUtils {
 			int destWidth = (int) (srcBufferedImage.getWidth(null) * scale / 100.0);
 			int destHeight = (int) (srcBufferedImage.getHeight(null) * scale / 100.0);
 
-			BufferedImage destBufferedImage = new BufferedImage(destWidth,
-					destHeight, BufferedImage.TYPE_INT_RGB);
+			BufferedImage destBufferedImage = new BufferedImage(destWidth, destHeight, BufferedImage.TYPE_INT_RGB);
 			Graphics2D graphics2D = destBufferedImage.createGraphics();
 			graphics2D.setBackground(Color.WHITE);
 			graphics2D.clearRect(0, 0, destWidth, destHeight);
-			graphics2D.drawImage(srcBufferedImage.getScaledInstance(destWidth,
-					destHeight, Image.SCALE_SMOOTH), 0, 0, null);
+			graphics2D.drawImage(srcBufferedImage.getScaledInstance(destWidth, destHeight, Image.SCALE_SMOOTH), 0, 0, null);
 			graphics2D.dispose();
 			return destBufferedImage;
 		} catch (IOException e) {
-			logger.error("CP_WatermarkUtils -> zoomImage 异常:", e);
+			System.out.println("CP_WatermarkUtils -> zoomImage 异常:" + e);
 		}
 		return null;
 	}
@@ -393,19 +351,15 @@ public class ImageUtils {
 		String targetPathOfText1 = "src/test/resources/textTest1.png";
 		String targetPathOfText2 = "src/test/resources/textTest2.png";
 		String logoIcon = "src/test/resources/nq.png";
-		ImageUtils.waterMark(logoText, srcImgPath, targetPathOfText1,
-				-45, 0.2f, ImageUtils.TYPE_TEXT);
+		ImageUtils.waterMark(logoText, srcImgPath, targetPathOfText1, -45, 0.2f, ImageUtils.TYPE_TEXT);
 
-		ImageUtils.waterMark(logoText, srcImgPath, targetPathOfText2, 0,
-				0.2f, ImageUtils.TYPE_TEXT);
+		ImageUtils.waterMark(logoText, srcImgPath, targetPathOfText2, 0, 0.2f, ImageUtils.TYPE_TEXT);
 
 		String targerPathOfIcon1 = "src/test/resources/iconTest1.png";
 		String targerPathOfIcon2 = "src/test/resources/iconTest2.png";
-		ImageUtils.waterMark(logoIcon, srcImgPath, targerPathOfIcon1,
-				-45, 0.2f, ImageUtils.TYPE_IMAGE);
+		ImageUtils.waterMark(logoIcon, srcImgPath, targerPathOfIcon1, -45, 0.2f, ImageUtils.TYPE_IMAGE);
 
-		ImageUtils.waterMark(logoIcon, srcImgPath, targerPathOfIcon2, 0,
-				0.2f, ImageUtils.TYPE_IMAGE);
+		ImageUtils.waterMark(logoIcon, srcImgPath, targerPathOfIcon2, 0, 0.2f, ImageUtils.TYPE_IMAGE);
 	}
 
 }
--- src/main/java/com/smart/utils/ValidataXMLUtil.java
+++ src/main/java/com/smart/utils/ValidataXMLUtil.java
@@ -13,8 +13,6 @@ import org.dom4j.io.SAXReader;
 import org.dom4j.io.SAXValidator;
 import org.dom4j.io.XMLWriter;
 import org.dom4j.util.XMLErrorHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * 
@@ -29,17 +27,16 @@ import org.slf4j.LoggerFactory;
  */
 public class ValidataXMLUtil {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(ValidataXMLUtil.class);
-	
 	/**
 	 * 通过XSD（XML Schema）校验XML
-	 * @param 	xmlString  	将xml拼成的String
-	 * @param 	xsdFile 	schema路径
-	 * @return	true		验证成功	,	false		验证失败
+	 * 
+	 * @param xmlString
+	 *            将xml拼成的String
+	 * @param xsdFile
+	 *            schema路径
+	 * @return true 验证成功 , false 验证失败
 	 */
-	public static boolean validateXMLByXSD(String xmlString, String xsdFileName)
-			throws Exception {
+	public static boolean validateXMLByXSD(String xmlString, String xsdFileName) throws Exception {
 		// 创建默认的XML错误处理器
 		XMLErrorHandler errorHandler = new XMLErrorHandler();
 		// 获取基于 SAX 的解析器的实例
@@ -54,14 +51,12 @@ public class ValidataXMLUtil {
 		SAXReader xmlReader = new SAXReader();
 		// 获取要校验xml文档实例
 		Document xmlDocument = xmlReader.read(new StringReader(xmlString));
-		// Document xmlDocument = (Document) xmlReader.read(new File(xmlFileName));
+		// Document xmlDocument = (Document) xmlReader.read(new
+		// File(xmlFileName));
 		// 设置 XMLReader 的基础实现中的特定属性。核心功能和属性列表可以在
 		// [url]http://sax.sourceforge.net/?selected=get-set[/url] 中找到。
-		parser.setProperty(
-				"http://java.sun.com/xml/jaxp/properties/schemaLanguage",
-				"http://www.w3.org/2001/XMLSchema");
-		parser.setProperty(
-				"http://java.sun.com/xml/jaxp/properties/schemaSource","file:" + getFilePath(xsdFileName));
+		parser.setProperty("http://java.sun.com/xml/jaxp/properties/schemaLanguage", "http://www.w3.org/2001/XMLSchema");
+		parser.setProperty("http://java.sun.com/xml/jaxp/properties/schemaSource", "file:" + getFilePath(xsdFileName));
 		// 创建一个SAXValidator校验工具，并设置校验工具的属性
 		SAXValidator validator = new SAXValidator(parser.getXMLReader());
 		// 设置校验工具的错误处理器，当发生错误时，可以从处理器对象中得到错误信息。
@@ -73,21 +68,21 @@ public class ValidataXMLUtil {
 		// 如果返回false，说明校验失败，打印错误信息
 		if (errorHandler.getErrors().hasContent()) {
 			writer.write(errorHandler.getErrors());
-			logger.error("XML文件校验失败！错误信息是："+errorHandler.getErrors());
+			System.out.println("XML文件校验失败！错误信息是：" + errorHandler.getErrors());
 			return false;
 		} else {
-			logger.info("XML文件验证通过!  XSD文件校验成功！");
+			System.out.println("XML文件验证通过!  XSD文件校验成功！");
 			return true;
 		}
 	}
 
-	public static String getFilePath(String xsdFileName){
+	public static String getFilePath(String xsdFileName) {
 		URL xsdFilePath = ValidataXMLUtil.class.getClassLoader().getResource("");
 		String xsdFile = xsdFilePath.getFile();
 		File file = new File(xsdFile);
 		xsdFile = file.getPath();
-//		System.out.println( xsdFile + File.separator + xsdFileName);
-		return   xsdFile + File.separator + xsdFileName;
+		// System.out.println( xsdFile + File.separator + xsdFileName);
+		return xsdFile + File.separator + xsdFileName;
 	}
-	
+
 }
